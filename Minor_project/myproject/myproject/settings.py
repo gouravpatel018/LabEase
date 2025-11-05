6c143b13b87e3ab62943e07bc23fc44bdecf9cc9
gourav <pgourav3859@gmail.com>
2025-11-05 12:54:14 +0530

    env file updated

--- Minor_project/myproject/myproject/settings.py
+++ Minor_project/myproject/myproject/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$l*4t^ci8q=e33s!z(b&*@n#dmi60jh@35_*k%a1li@5f()g3_'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
